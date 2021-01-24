7053b1aad67de31f10b44fbd97c3acb1c0f770b7
alexander-achilles <mrinmoym247@gmail.com>
2021-01-24 17:24:18 +0530

    Prepared for deployment

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI,{ useNewUrlParser: true , useUnifiedTopology: true})
   .then(result => {
-    app.listen(3000,()=>{console.log("server is up")});
+    app.listen(process.env.PORT || 3000,()=>{console.log("server is up")});
   })
   .catch(err => {
     console.log(err);
